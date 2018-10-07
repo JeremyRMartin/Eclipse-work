66aa885f01f75be4b1ca707a1da2ff98557deae7
JeremyRMartin <42840400+JeremyRMartin@users.noreply.github.com>
2018-10-07 14:36:16 -0400

    Initial Commit

--- Integration Project/src/Main.java
+++ Integration Project/src/Main.java
@@ -1,33 +1,45 @@
-import java.util.Scanner;
-//Jeremy Martin
-/*Project Description:
- * My project will be a simple 2D RPG game. 
- * That manages an inventory and items.
- * Eventually.
+import Storyline.Credits;
+import Storyline.Intro;
+import Storyline.chapter1;
+import tools.Utility;
+
+/* Jeremy Martin
+ * Project Description:
+ *
+ * My project will be a simple game
+ * involving choices and a story line.
+ * 
  */
 public class Main {
 
-	public static void main(String[] args) {
-		//Welcome the User
-		System.out.println("Welcome to my integration Project.");
-		System.out.println("Are you here to watch me crash and burn, while I writhe around in confusion? (*HINT* use Y/N) ");
+	public static void main(String[] args) throws InterruptedException {
 		
-		//init vars
-		String answer;
-			
-		//init scanner and read users response
-		Scanner scan = new Scanner(System.in);
-		answer = scan.nextLine();
-		scan.close();
-		
-		
-		//test response, and reply
-		if(answer.equals("Y")) {
-			System.out.println("You monster, How could you!");
-		}else if(answer.equals("N")) {
-			System.out.println("Awesome, thank goodness you're here.");
-		}else {
-			System.out.println("You okay buddy? The instructions were simple.");
+		boolean invalidInput = true;
+		String menuSelection;
+		menuSelection = Intro.printIntro();
+		while (invalidInput) {
+			switch(menuSelection.toUpperCase()) {
+			case "YES":
+				chapter1.section1();
+				invalidInput = false;
+				break;
+			case "NO":
+				Credits.rollFalseCredits();
+				invalidInput = false;
+				break;
+			case "MORE INFO":
+				menuSelection = Intro.moreInfo();
+				break;
+			default:
+				System.out.println("PLEASE ENTER A CORRECT OPTION");
+				menuSelection = Utility.scanner.nextLine();
+				
 			}
 		}
-	}
+		
+		
+		
+		
+		
+	}//end main method
+}//end Main class
